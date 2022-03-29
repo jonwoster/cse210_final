5a638450fbe1409a7cf54bba78096a2ed83a1475
Jonathan Woster <jonathanwoster@gmail.com>
2022-03-29 09:43:07 -0500

    some clean up and comments

--- aliens/game/scripting/handle_collisions_action.py
+++ aliens/game/scripting/handle_collisions_action.py
@@ -36,7 +36,6 @@ class HandleCollisionsAction(Action):
         """
         if not self._is_game_over:
             self._handle_segment_collision(cast)
-            self._handle_game_over(cast)
 
     
     def _handle_segment_collision(self, cast):
@@ -52,68 +51,57 @@ class HandleCollisionsAction(Action):
         # Get the list of aliens
         alien_group = cast.get_first_actor("aliens")
         aliens = alien_group.get_aliens()
-
         
-
-        
-            # loop through the aliens
+        # loop through the aliens
         for alien in aliens:
-                # if the Y posiiton of the current alien reaches max Y, then set game over flag
-                # remove all items from the alien list
-            
+
+            # if the Y posiiton of the current alien reaches max Y, then set game over flag
+            # remove all items from the alien list
             if alien.get_position().get_y() >= constants.BOTTOM_SCREEN:
                 self._is_game_over = True
                 aliens.clear()  # remove all aliens
 
-                
+                # set up position for game over message
                 x = int(constants.MAX_X / 2)
                 y = int(constants.MAX_Y / 2)
                 position = Point(x, y)
+                # set up message for game over
                 message = Actor()
-                #if alien touch the bottom 
                 message.set_text("Game Over!" " *** " "You Lost!!!")
-            
                 message.set_position(position)
                 cast.add_actor("messages", message)
                 #play sound
                 raylib.PlaySound(lose_sound)
                 break
 
+            # get the list of bullets
             bullets = bullet_group.get_bullets()
             
             #loop through the bullets
             for bullet in bullets:
 
                 # if position of current bullet equals position of current alien, remove both the alien and the bullet
-
                 if bullet.get_position().equals(alien.get_position()):
-
                     bullets.remove(bullet)
                     aliens.remove(alien)
 
-
                 # if Y position of the current bullet reaches the top of screen, then remove the bullet 
                 # so it doesn't come back up from the bottom
-    
                 if (bullet.get_position().get_y()) <= (constants.MIN_Y):
                     bullets.remove(bullet)
 
-
-
-            # if the count of aliens reaches 0, then set game over flag and exit method
-
+            # if the count of aliens reaches 0, then set game over flag
             if len(aliens) == 0:
                 self._is_game_over = True
                 bullets.clear()  # remove all bullets
+
+                # setup the location of game over message
                 x = int(constants.MAX_X / 2)
                 y = int(constants.MAX_Y / 2)
                 position = Point(x, y)
+                # setup the message for game over, indicating player won
                 message = Actor()
-                
-                
-                #if player kill aliens
                 message.set_text("Game Over! "" *** "" You Win!!!")
-                
                 message.set_position(position)
                 cast.add_actor("messages", message)
 
